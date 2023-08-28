dc04eb743bd6da26c78dc609e1ebdd83607897e9
Michal Sojka <michal.sojka@cvut.cz>
2023-08-28 18:04:22 +0200

    Prevent dlopen error when loading rviz plugin

--- rviz_carla_plugin/src/carla_control_panel_ROS2.cpp
+++ rviz_carla_plugin/src/carla_control_panel_ROS2.cpp
@@ -22,6 +22,8 @@
 #include <chrono>
 #include <iomanip>
 
+// Workaround ros2/geometry2#242
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>  // NOLINT(build/include_order)
 #include <tf2/utils.h>
 
 #include <OgreCamera.h>
