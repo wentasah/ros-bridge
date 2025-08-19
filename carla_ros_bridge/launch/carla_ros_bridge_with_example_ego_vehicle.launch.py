3e94e8693e9a1617a102ac27418750b90eb6abc1
toamz <39005598+toamz@users.noreply.github.com>
2025-08-19 16:18:17 +0200

    Add option "passive_load_town" to load town even with passive mode

--- carla_ros_bridge/launch/carla_ros_bridge_with_example_ego_vehicle.launch.py
+++ carla_ros_bridge/launch/carla_ros_bridge_with_example_ego_vehicle.launch.py
@@ -39,6 +39,10 @@ def generate_launch_description():
             default_value='False'
         ),
         launch.actions.DeclareLaunchArgument(
+            name='passive_load_town',
+            default_value='False',
+        ),
+        launch.actions.DeclareLaunchArgument(
             name='synchronous_mode_wait_for_vehicle_control_command',
             default_value='False'
         ),
@@ -57,6 +61,7 @@ def generate_launch_description():
                 'town': launch.substitutions.LaunchConfiguration('town'),
                 'timeout': launch.substitutions.LaunchConfiguration('timeout'),
                 'passive': launch.substitutions.LaunchConfiguration('passive'),
+                'passive_load_town': launch.substitutions.LaunchConfiguration('passive_load_town'),
                 'synchronous_mode_wait_for_vehicle_control_command': launch.substitutions.LaunchConfiguration('synchronous_mode_wait_for_vehicle_control_command'),
                 'fixed_delta_seconds': launch.substitutions.LaunchConfiguration('fixed_delta_seconds')
             }.items()
